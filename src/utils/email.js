9ad579637039e5e6e18ccccdcf97959d520c208e
ahmed0ty <ahmedabdelbaky2321@gmail.com>
2026-04-01 07:26:29 +0200

    last commit

--- src/utils/email.js
+++ src/utils/email.js
@@ -40,13 +40,16 @@ const createTransporter = () => {
     return null;
   }
 
-  return nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+return nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 };
 
 export const sendEmail = async ({ to, subject, html }) => {
