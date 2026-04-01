8959c56d023e72675d661c218f06d915de17e0b4
ahmed0ty <ahmedabdelbaky2321@gmail.com>
2026-04-01 14:37:27 +0200

    last commit

--- src/middlewares/rateLimiter.middleware.js
+++ src/middlewares/rateLimiter.middleware.js
@@ -1,13 +1,25 @@
 import rateLimit from "express-rate-limit";
 
-// حماية الـ login و forgot-password
+// export const authLimiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 5, 
+//   message: {
+//     success: false,
+//     message: "Too many requests from this IP, please try again after 15 minutes"
+//   },
+//   standardHeaders: true, 
+//   legacyHeaders: false,  
+// });
+
 export const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 دقيقة
-  max: 50, // أقصى 5 طلبات لكل IP خلال 15 دقيقة
-  message: {
-    success: false,
-    message: "Too many requests from this IP, please try again after 15 minutes"
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    res.status(429).json({
+      success: false,
+      message: "Too many login attempts, try again after 15 minutes"
+    });
   },
-  standardHeaders: true, // يضيف X-RateLimit-* headers
-  legacyHeaders: false,  // يمنع X-RateLimit-* القديم
 });
